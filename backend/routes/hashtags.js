2245552b7e4518303d78758a6d4566ac39f8e7c5
SoumayaL <soumaya.laajaj@hotmail.fr>
2022-11-24 18:12:28 +0100

    tentative de route addtweet mais en vain

--- backend/routes/hashtags.js
+++ backend/routes/hashtags.js
@@ -7,13 +7,13 @@ const Hashtag = require('../models/hashtag');
 
 // GET # dans trends
 router.get('/hash', (req, res) => {
- Hashtag.find({ hash: req.params.hash}).then(data => {
+  Hashtag.find({ hash: req.params.hash}).then(data => {
     if (data) {
         res.json({ result: true, hash: data.name });
       } else {
         res.json({ result: false, error: 'No # in bdd' });
       }
- })
+})
 })
 
 module.exports = router;
