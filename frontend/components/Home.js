e7416099e3f5d78e39d3ab2bfa818662e2d190ce
Alexis <alexis94440@orange.fr>
2022-11-25 14:48:00 +0100

    Front à deux

--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -1,14 +1,31 @@
 import styles from '../styles/Home.module.css';
+import logo from '../public/logo.png';
+import Tweet from './Tweet';
 
 function Home() {
   return (
-    <div>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </main>
+    
+  <div className={styles.container}>
+    <div className={logo}>
+      <button></button>
+      <img src="logo.png" alt="image"/>
+
+      </div>
+    <div className={styles.home}>
+      <div>
+      <h4>Home</h4>
+      <input className="tweet" type="text" placeholder="What's up ?"/>
+      <span>0/280</span>
+      <button>Tweet</button>
+      </div>
+      <Tweet/>
+      <div>
+      </div>
     </div>
+    <div className={styles.trends}>Trends</div>
+  </div>
+
+    
   );
 }
 
